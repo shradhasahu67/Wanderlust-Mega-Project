9bb56fc64ae4bb79bc559d616125f0da7709e2ba
Shradha <shradhasahu67@gmail.com>
2026-08-19 03:41:14 +0530

    Update Jenkins pipeline and infrastructure configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-085f9c64a9b75eed5"
+  default     = "ami-0b6d9d3d33ba97d99"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
-  default     = "t2.large"
+  default     = "m7i-flex.large"
 }
